db5ca5f8d557fe8ddebbeb64a5d45f9f6e62c543
Blinc <buta.cristi14@gmail.com>
2022-07-12 01:55:40 +0300

    removed console.log

--- src/tests/deployment.ts
+++ src/tests/deployment.ts
@@ -6,7 +6,6 @@ describe("Deployment", async function () {
     const Contract = await ethers.getContractFactory("Contract");
     const contract = await Contract.deploy("This is a hardhat boilerplate!");
     await contract.deployed();
-    console.log("contract.address:", contract.address);
     expect(contract.address.length).to.equal(42);
   });
 });
